f615a2510e8521b98d81ad02ed3c2457fa610bef
Alexis <2175497@cegepmontpetit.ca>
2024-08-30 10:29:50 -0400

    FCT Games et Genres

--- Models/Game.cs
+++ Models/Game.cs
@@ -1,10 +1,21 @@
-﻿namespace RelationsNaN.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace RelationsNaN.Models
 {
     public class Game
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public string Image { get; set; }
+
         public int ReleaseYear { get; set; }
+
+
+        public int? GenreId { get; set; }
+
+        [ValidateNever]
+        public Genre? Genre { get; set; }
     }
 }
